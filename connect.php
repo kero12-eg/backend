4c9d2f63d8a44ffe3e6c1d640670cf5e939a76b7
kero12-eg <madk5078@gmail.com>
2026-05-26 02:35:11 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,32 @@
+<?php
+
+$host = "localhost";
+
+$dbname = "lawyer_system";
+
+$user = "root";
+
+$pass = "";
+
+try {
+
+$con = new PDO(
+
+"mysql:host=$host;dbname=$dbname",
+
+$user,
+
+$pass
+);
+
+$con->setAttribute(
+
+PDO::ATTR_ERRMODE,
+
+PDO::ERRMODE_EXCEPTION
+);
+
+} catch(PDOException $e) {
+
+echo $e->getMessage();
+}
